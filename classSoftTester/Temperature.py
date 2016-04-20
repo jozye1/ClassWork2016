d80bf6853a5a1bf750ce7cbd2714f1769a23ae57
Josue Merisier <ijosueko@gmail.com>
2016-04-20 17:29:37 -0400

    Created Temperature.py

---
+++ classSoftTester/Temperature.py
@@ -0,0 +1,24 @@
+def TodaysTemp():
+    fahrenheit = 0
+    celsius = float((fahrenheit - 32) / 1.8)
+    fahrenheit = float((1.8 * celsius) + 32)
+
+    continue_entry = True
+
+    while continue_entry == True:
+        temperature = float(input("Please enter today's temperature in Fahrenheit: "))
+        celsius = (temperature - 32) / 1.8
+        print(temperature,end=" ")
+        print("degree(s) Fahrenheit = " ,end="")
+        print(celsius,end="")
+        print("degree(s) Celsius.")
+        proceed = str(input("Do you wish to continue, Y or N? "))
+        if proceed == "Y" or proceed== "y":
+            continue_entry = True
+        else:
+            continue_entry = False
+
+    print("")
+    print("Thank you for your participation! Have a great day!!!")
+
+TodaysTemp()
