d203eb47b2f0a2d54f9418b1b73eb6369d9b20e2
Josue Merisier <ijosueko@gmail.com>
2016-04-21 13:25:40 -0400

    Created Temperature.py

---
+++ classSoftTester/Temperature2.py
@@ -0,0 +1,13 @@
+def weatherIncrement():
+
+    print("")
+    print("This program prints Celsius temperatures from 0-100, in increment of 5, along with the equivalents in Fahrenheit: ")
+    print("")
+
+    temp = 0
+    for t in range(0,101,5):
+        weatherTempf = (1.8 * temp) + 32
+        print(temp, " Celsius is: " "\t", round(weatherTempf, 2), " Fahrenheit:")
+        temp += 5
+
+weatherIncrement()
