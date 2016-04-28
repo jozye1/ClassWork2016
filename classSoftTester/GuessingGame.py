136d1ad22e3e8a81fe98683a652790bfb05fd96b
Josue Merisier <ijosueko@gmail.com>
2016-04-28 15:55:22 -0400

    Created Temperature.py

--- classSoftTester/GuessingGame.py
+++ classSoftTester/GuessingGame.py
@@ -13,27 +13,36 @@ def guessingGame():
     #userInput = int(input("Please enter a guessed number between 1 and 10:  "))
     guessedNumber = random.randint(1, 10)
     #print(random.randint(1, 10))
-    global count
+    global count,counter
     count = 0
-    #counter = 0
+    counter = 3
+
+    print("Welcome to our number-guessing game! You have three attempts at guessing the correct number.")
+
     value = True
     while value == True:
         count += 1
-        for guessed in range(count+1):
+        for guessed in range(count):
             print("")
             userInput = int(input("Please enter a guessed number between 1 and 10:  "))
             print("")
-
+            counter -= 1
             if guessedNumber == userInput:
             #print(random.randint(1, 10))
                 print("")
-                print("WOW, Outstanding! You have guessed the random number correctly: ", guessedNumber)
+                print("WOW, Outstanding! You have guessed the random number correctly, which is: ", guessedNumber)
                 value = False
-
-            elif userInput != guessedNumber or count != 3:
-                count += 1
-                print("Oh nooo, you almost had it! Please try again! You've got ", count, "more times to try.")
+                break
+            #elif userInput != guessedNumber:
+                print("Oh nooo, you almost had it! Please try again! You've got ", counter, "more times to try.")
                 print("")
+            elif count == 3:
+                print("Oh nooo, you almost had it! Please try again! You've got ", counter, "more times to try.")
+                print("")
+                print("counter proof: ", count)
+                print("You have exhausted all your attempts. Please go home and think about it, or give us a call to reset your account.")
+                value = False
+                break
             else:
                 print("")
                 print("It is too bad you could not guessed the right number.")
@@ -41,6 +50,6 @@ def guessingGame():
                 value = False
                 break
     print("")
-    print("Thank you for having fun with us! Please come back again soon!")
+    print("Thank you for having stopped by & have fun with us! Please come back again soon!")
 
 guessingGame()
--- classSoftTester/UserInput.py
+++ classSoftTester/UserInput.py
@@ -4,16 +4,16 @@
 # Author: Josue Merisier
 # Date: 22 April 2016
 
-def userInput():
-
-    count = 0
-    #global counter
-    counter = 0
+def userInputMain():
+    global iterationValue, goodData, enteredValues
     enteredValues = []
-    global iterationValue
     iterationValue = 0
-    #value = True
+#userInputMain()
 
+#def acceptableData():
+    global count
+    count = 0
+    counter = 0
     goodData = True
     while goodData == True:
         counter += 1
@@ -25,23 +25,27 @@ def userInput():
         except:
             print("")
             print("This program only accepts numeric characters. ")
-            if count == 0:
+            if count == 1:
                 counter = 3
-                count = 1
-                print("You have ", counter, " more chances to try again.")
-            elif count == 1:
-                counter = 2
                 count = 2
-                print("You have ", counter, " more chance to try again.")
+                print("You have ", counter, " more chances to try again.")
             elif count == 2:
+                counter = 2
+                count = 3
+                print("You have ", counter, " more chance to try again.")
+            elif count == 3:
                 print("1) We regret that you were not able to able to play & enjoy the game. Come back later.")
                 break
                 return
 
+        if iterationValue <= 0:
+            return
+#acceptableData()
 
-    if iterationValue <= 0:
-        return
-
+#def iterationCalculations():
+    #global count
+    count = 0
+    counter = 0
     while iterationValue >= 0:
         counter += 1
         try:
@@ -53,26 +57,22 @@ def userInput():
         except:
             print("")
             print("This program only accepts numeric characters. ")
-            if count == 0:
+            if count == 1:
                 counter = 3
                 print("You have ",counter," more chances to try again.")
-            elif count == 1:
+            elif count == 2:
                 counter = 2
                 print("You have ", counter, " more chance to try again.")
-            elif count == 2:
+            elif count == 3:
                 print("2) We regret that you were not able to able to play & enjoy the game. Come back later.")
-                #value = False
                 break
 
-        #if iterationValue >= count or iterationValue == count or iterationValue < count:
-            #value = False
-                #return
-        #goOn = str(input("Do you wish to continue? Enter 'Q' or 'q' to quit, otherwise enter any other character to continue: "))
         if iterationValue <= 0:
-            #value = False
             return
-        #else:
-           # value = True
+#iterationCalculations()
+userInputMain()
+
+def printedResults():
     print("")
     print("This is the list of entered numbers: ", end="")
 
@@ -87,4 +87,33 @@ def userInput():
     print("The minimum or least number entered in the list is: ", enteredValues[0])
     print("The maximum or largest number entered in the list is: ", enteredValues[iterationValue -1])
 
-userInput()
+printedResults()
+
+def repeatingProcess():
+    keepingGoing = True
+    while keepingGoing == True:
+        print("")
+        goOn = str(input("Do you wish to continue? Enter 'Q' or 'q' to quit, otherwise enter any other character to continue: "))
+        print("")
+        if goOn == str("Q") or goOn == str("q"):
+            goodData = False
+            keepingGoing = False
+            return
+        else:
+            goodData = True
+            #acceptableData()
+            #iterationCalculations()
+            userInputMain()
+            printedResults()
+            #keepingGoing = True
+
+repeatingProcess()
+
+print("")
+print("Thank you for taking part in our promotion! Please come back soon! Have a great day!!!")
+
+
+
+
+
+
